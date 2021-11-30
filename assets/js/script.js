4f6ea9a64d8c2b3db634e6221751cd85fb57eeb1
fnurrahmah125 <fnurrahmah125@gmail.com>
2021-11-30 22:57:05 +0700

    finish the redesign dropbox web

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,29 @@
-// ------------ Landing Page ------------ //
-const menuToggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector(".nav-menu");
-
-menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
-});
-
-// ------------ Dashboard Page ------------ //
-
-// middle column
-const menuToggleDb = document.querySelector(".search-box div .menu-toggle");
+// ------------ Menu Toggle ------------ //
+const menuToggleDb = document.querySelector(".search-box  .menu-toggle");
 const colLeft = document.querySelector(".col-left");
-const background = document.querySelector(".bg-transparent");
+const bgLeft = document.querySelector(".bg-left");
 
 menuToggleDb.addEventListener("click", function () {
-  colLeft.style.transform = "translateX(0)";
-  background.style.background = "rgba(0, 0, 0, 0.5)";
-  background.style.zIndex = "1";
+  colLeft.classList.add("col-left-show");
+  bgLeft.classList.add("bg-show");
 });
 
-background.addEventListener("click", function () {
-  colLeft.style.transform = "translateX(-100%)";
-  background.style.background = "rgba(0, 0, 0, 0)";
-  background.style.zIndex = "-1";
+bgLeft.addEventListener("click", function () {
+  colLeft.classList.remove("col-left-show");
+  bgLeft.classList.remove("bg-show");
 });
 
-// mobile version
+// ------------ Add Button ------------ //
 const addIcon = document.querySelector(".add-icon");
-const background2 = document.querySelector(".bg-transparent-2");
+const bgDown = document.querySelector(".bg-down");
 const addContainer = document.querySelector(".add-container");
 
 addIcon.addEventListener("click", function () {
-  background2.style.background = "rgba(0, 0, 0, 0.5)";
-  background2.style.zIndex = "1";
-
-  addContainer.style.transform = "translateY(0)";
-  addContainer.style.zIndex = "2";
+  bgDown.classList.add("bg-show");
+  addContainer.classList.add("add-container-show");
 });
 
-background2.addEventListener("click", function () {
-  background2.style.background = "rgba(0, 0, 0, 0)";
-  background2.style.zIndex = "-1";
-
-  addContainer.style.transform = "translateY(300%)";
-  addContainer.style.zIndex = "-1";
+bgDown.addEventListener("click", function () {
+  bgDown.classList.remove("bg-show");
+  addContainer.classList.remove("add-container-show");
 });
